0aab7b4beaebd61d6ab2cddb32d72868aa9e79b0
tobenna <eliyahscopy@gmail.com>
2022-11-15 14:24:59 +0100

    update

---
+++ pages/index.js
@@ -0,0 +1,48 @@
+import Image from "next/image"
+import styles from "../styles/Home.module.css"
+import { useMoralis, useMoralisQuery } from "react-moralis"
+import NFTBox from "../components/NFTBox"
+import networkMapping from "../constants/networkMapping.json"
+import { useQuery } from "@apollo/client"
+import GET_ACTIVE_ITEMS from "../constants/subgraphQueries"
+
+export default function Home() {
+    // This is our home page where we list our nft
+    // How do we list them
+    // we use a centralized database and listen to our event(itemListed)
+    // There is a decentralized method known as the Graph
+    const { isWeb3Enabled, chainId } = useMoralis()
+    const chainString = chainId ? parseInt(chainId).toString() : "31337"
+    const marketplaceAddress = networkMapping[chainString].NftMarketplace[0]
+    const { loading, error, data: listedNfts } = useQuery(GET_ACTIVE_ITEMS)
+
+    return (
+        <div className="container mx-a uto">
+            <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
+            <div className="flex flex-wrap">
+                {isWeb3Enabled ? (
+                    loading || !listedNfts ? (
+                        <div>Loading...</div>
+                    ) : (
+                        listedNfts.activeItems.map((nft) => {
+                            console.log(nft)
+                            const { price, nftAddress, tokenId, seller } = nft
+                            return (
+                                <NFTBox
+                                    price={price}
+                                    nftAddress={nftAddress}
+                                    tokenId={tokenId}
+                                    marketplaceAddress={marketplaceAddress}
+                                    seller={seller}
+                                    key={`${nftAddress}${tokenId}`}
+                                />
+                            )
+                        })
+                    )
+                ) : (
+                    <div>Web3 Currently Not Enabled</div>
+                )}
+            </div>
+        </div>
+    )
+}
