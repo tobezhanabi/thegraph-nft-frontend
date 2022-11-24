0c1054b3013e18da536b1d65292cb0183b2d80c9
tobenna <eliyahscopy@gmail.com>
2022-11-24 14:29:51 +0100

    frontend

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
     const { isWeb3Enabled, chainId } = useMoralis()
     const chainString = chainId ? parseInt(chainId).toString() : "31337"
     const marketplaceAddress = networkMapping[chainString].NftMarketplace[0]
-    const { loading, error, data: listedNfts } = useQuery(GET_ACTIVE_ITEMS)
+    const { loading, error, data } = useQuery(GET_ACTIVE_ITEMS)
 
     return (
         <div className="container mx-a uto">
